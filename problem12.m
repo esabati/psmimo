84ffdeccda161f401ce0839d91041d48f94fe50f
esabatini <eugenio.sabatini@usp.br>
2022-06-07 14:02:08 +0200

    minor changes in problem12

--- problem12.m
+++ problem12.m
@@ -1,9 +1,14 @@
 function [V_new,t,u] = problem12(V,V0,a,h,tau,rho,Pc,sigma2,Pmax,N,M,lambda)
 
-cvx_begin sdp
-    variable V_new semidefinite
+cvx_begin
+    variable V_new(N,N) semidefinite
     variable t
     variable u
+    maximize(t)
+    subject to
+        (lambda/2)*t^2 + (1/(2*lambda))*u^2 <= 
+        u >= (1/rho)*
+    
 cvx_end
 end
 
