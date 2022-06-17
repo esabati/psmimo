1ee5660eb087377b65d2ef96aa7d8a1d566e62ad
esabatini <eugenio.sabatini@usp.br>
2022-06-17 04:15:27 +0200

    finished code

--- algorithm1.m
+++ algorithm1.m
@@ -1,24 +1,24 @@
-function [V0,V1,V2] = algorithm1(N,M,a,h,tau,sigma2,gamma,rho,Pc,Pmax)
-V0_old = zeros(N,N);
-V1_old = zeros(N,N);
-V2_old = zeros(N,N);
+function [V] = algorithm1(K,N,M,a,h,tau,sigma2,gamma,rho,Pc,Pmax,epsilon)
+V_old = zeros(N,N,K + 1);
+for k = 1:1+K
+    v_rand = ones(N,1).*exp(1j*2*pi*rand(N,1))/sqrt(N)*sqrt(Pmax/(1+K));
+    V_old(:,:,k) = v_rand*v_rand';
+end
+
 lambda = 1;
 l = 0;
 
-while l < 10
+t_old = 0;
+
+while(1)
     l = l + 1;
-    [V0,V1,V2,t,u] = problem12(N,M,a,h,tau,sigma2,gamma,rho,Pc,Pmax,lambda,V0_old,V1_old,V2_old);
-    if isnan(u) || isnan(t)
-        V0 = V0_old;
-        V1 = V1_old;
-        V2 = V2_old;
-        disp('out');
-        break
-    end
-    V0_old = V0;
-    V1_old = V1;
-    V2_old = V2;
+    [V,t,u] = problem12(K,N,M,a,h,tau,sigma2,gamma,rho,Pc,Pmax,lambda,V_old);
+    V_old = V;
     
     lambda = u/t;
+    if abs(t - t_old) < epsilon
+        break
+    end
+    t_old = t;
 end
 end
--- problem12.m
+++ problem12.m
@@ -1,38 +1,40 @@
-function [V0,V1,V2,t,u] = problem12(N,M,a,h,tau,sigma2,gamma,rho,Pc,Pmax,lambda,V0_old,V1_old,V2_old)
-a1 = log(conj(h(:,1))'*V0_old*h(:,1) + conj(h(:,1))'*V2_old*h(:,1) + sigma2(1))/log(2); % auxiliary variable for R1
-a2 = log(conj(h(:,2))'*V0_old*h(:,2) + conj(h(:,2))'*V1_old*h(:,2) + sigma2(2))/log(2); % auxiliary variable for R2
-
-disp('in');
-cvx_begin
+function [V,t,u] = problem12(K,N,M,a,h,tau,sigma2,gamma,rho,Pc,Pmax,lambda,V_old)
+cvx_begin sdp
     variable t
     variable u
-    variable V0(N,N) semidefinite
-    variable V1(N,N) semidefinite
-    variable V2(N,N) semidefinite
-    
-    R1_aux = log(conj(h(:,1))'*V0*h(:,1) + conj(h(:,1))'*V1*h(:,1) + conj(h(:,1))'*V2*h(:,1) + sigma2(1))/log(2); % pre-calculation for R1
-    R2_aux = log(conj(h(:,2))'*V0*h(:,2) + conj(h(:,2))'*V1*h(:,2) + conj(h(:,2))'*V2*h(:,2) + sigma2(2))/log(2); % pre-calculation for R2
-    
-    R1 = R1_aux - (a1 + ((log(exp(1))/log(2))/(2^a1))*(conj(h(:,1))'*(V0 - V0_old)*h(:,1) + conj(h(:,1))'*(V2 - V2_old)*h(:,1)));
-    R2 = R2_aux - (a2 + ((log(exp(1))/log(2))/(2^a2))*(conj(h(:,2))'*(V0 - V0_old)*h(:,2) + conj(h(:,2))'*(V1 - V1_old)*h(:,2)));
-    
-    maximize(t)
-    
+    variable V(N,N,1 + K) hermitian
+    maximize t
     subject to
-        (lambda/2)*t^2 + 1/(2*lambda)*u^2 <= R1 + R2 % (12)
+        sumV = zeros(N,N);
+        sumV_old = zeros(N,N);
         
-        u >= (1/rho)*(trace(V0) + trace(V1) + trace(V2)) + Pc % (7a)
-        
-        conj(h(:,1))'*V1*h(:,1) >= tau(1)*(conj(h(:,1))'*V0*h(:,1) + conj(h(:,1))'*V2*h(:,1) + sigma2(1)) % (7c) k = 1
-        conj(h(:,2))'*V2*h(:,2) >= tau(2)*(conj(h(:,2))'*V0*h(:,2) + conj(h(:,2))'*V1*h(:,2) + sigma2(2)) % (7c) k = 2
-        
-        trace(V0) + trace(V1) + trace(V2) <= Pmax % (7d)
-        
-        for m = 1:M
-            (a(:,m)'*(V0 + V1 + V2)*a(:,m)) >= gamma(m) % (7e)
+        for k = 1:K + 1
+            sumV = sumV + V(:,:,k);
+            sumV_old = sumV_old + V_old(:,:,k);
+            V(:,:,k) >= 0 
         end
         
-        t >= 0 % (7f)
-        u >= 0 % (7f)
+        t >= 0
+        u >= 0
+        
+        clear R_underline
+        for k = 1:K
+            a_k = log(quad_form(h(:,:,k),sumV_old - V_old(:,:,k)) + sigma2)/log(2); 
+            b_k = 1/(quad_form(h(:,:,k),sumV_old - V_old(:,:,k)) + sigma2)/log(2); 
+            R_underline(1,k) = log(quad_form(h(:,:,k),sumV) + sigma2)/log(2) - (a_k + b_k*quad_form(h(:,:,k),(sumV - V(:,:,k)) - (sumV_old - V_old(:,:,k))));
+        end
+        lambda/2*t^2 + 1/(2*lambda)*u^2 <= sum(R_underline);
+        
+        u >= trace(sumV)/rho + Pc
+        
+        for k = 1:K
+            quad_form(h(:,:,k),V(:,:,k)) >= tau*(quad_form(h(:,:,k),sumV - V(:,:,k)) + sigma2);
+        end
+        
+        trace(sumV) <= Pmax % (7d)
+        
+        for m = 1:M
+            quad_form(a(:,:,m),sumV) >= gamma % (7e)
+        end
 cvx_end
 end
